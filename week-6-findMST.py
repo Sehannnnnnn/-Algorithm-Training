e79bb7ac64ebc3ac9d93a3976fba77ef026ae0ef
rlatp1409 <stanley710@naver.com>
2021-10-17 16:03:56 +0900

    MST update

--- week-6-findMST.py
+++ week-6-findMST.py
@@ -1,4 +1,4 @@
-# 내림차순 정렬 함수
+# 오름차순 정렬 함수
 def quickSort_Ascending(arr):
     if len(arr) <= 1:
         return arr
@@ -13,15 +13,6 @@ def quickSort_Ascending(arr):
             equal.append(a)
     return quickSort_Ascending(left) + equal + quickSort_Ascending(right)
 
-
-def kruskal_MST(Edge_set, n):
-    quickSort_Ascending(Edge_set)
-    # 최소 가중치 +
-
-    # 순환하는 지 체크하고 버릴지 결정
-    return
-
-
 #실행
 node, edge = map(int, input().split())
 
@@ -29,7 +20,41 @@ edge_set = []
 for i in range(edge):
     edge_set.append(list(map(int, input().split())))
 
-print(edge_set)
-
+#edge 정렬
 edge_set = quickSort_Ascending(edge_set)
-print(edge_set)
+
+
+#find & union 알고리즘
+arr = [i for i in range(node+1)]
+def find(x):
+    if arr[x] != x:
+        return find(arr[x])
+    return x
+
+def union(a, b):
+    a = find(a)
+    b = find(b)
+    if a < b:
+        arr[b] = a
+    else: 
+        arr[a] = b
+
+
+#크루스컬 MST 알고리즘
+def kruskal_MST(Edge_set, n):
+    totalMST = 0
+    edgeCount = 0
+    for element in Edge_set:
+        if edgeCount == n-1:
+            break
+        nodeA, nodeB  = element[0],element[1]
+        edgelength = element[2]
+        if find(nodeA) != find(nodeB):
+            union(nodeA, nodeB)
+            totalMST += edgelength
+            edgeCount += 1
+    return totalMST
+
+
+
+print(kruskal_MST(edge_set, node))
